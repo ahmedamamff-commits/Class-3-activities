b6253af115f41e90bf2b9035cbd4f23c56b26626
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-07-21 10:55:37 -0700

    Add files via upload
    Type Casting Activity

---
+++ main2.py
@@ -0,0 +1,28 @@
+name = "Ahmed"
+age = 11
+Student_at_Codingal = True
+Favorite_Number = 67.5
+
+print("Name :", name)
+print("Data Type of Name is:", type(name))
+
+print("Age :", age)
+print("Data Type of Age is:", type(age))
+
+print("Student at Codingal:", Student_at_Codingal)
+print("Data Type of Student at Codingal is:", type(Student_at_Codingal))
+
+print("Favorite Number:", Favorite_Number)
+print("Data Type of Favorite Number is:", type(Favorite_Number))
+
+print("\n After Type Casting...")
+name = str(name)
+print(name)
+age = int(age)
+print(age)
+print("Data Type of age is:", type(age))
+Student_at_Codingal = bool(Student_at_Codingal)
+print(Student_at_Codingal)
+Favorite_Number = float(Favorite_Number)
+print(Favorite_Number)
+print("Data Type of Favorite Number is:", type(Favorite_Number))
